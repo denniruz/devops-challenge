94f9220856ad985ccd13c59d26e6b9af264b38da
Scott Holder <scott@iamscott.net>
2018-01-08 00:08:00 -0500

    Progress on API

--- rest api/ft.py
+++ rest api/ft.py
@@ -1,5 +1,7 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 from flask import Flask, jsonify, abort, request, make_response, url_for
+from collections import Counter
+import re
 
 app = Flask(__name__)
 
@@ -14,18 +16,36 @@ def index():
     return "Hello, World!"
 
 @app.route('/words', methods=['GET'])
-def get_tasks():
-    return jsonify({'words': words})
+def get_words():
+    print(words)
+
+    count = []
+
+    for key, values in words[0].items():
+            count[key] = len(values)
+    #return jsonify({'words': word})
+    return jsonify(count)
+    
+@app.route('/words/<string:countword>', methods=['GET'])
+def count_words(countword):
+    #return jsonify({'words': word})
+    count = str(words.count(countword))
+    returnjson = '{' + ' "{}" : {} '.format(countword, count) + '}'
+    print (returnjson)
+    return returnjson
 
 @app.route('/words', methods=['PUT'])
 def create_word():
     if not request.json or not 'word' in request.json:
         abort(400)
-    word = {
+    newword = {
         'word': request.json['word']
     }
-    words.append(word)
-    return jsonify({'words': word}), 201
+    if re.match('^\w+$', request.json['word']):
+        words.append(request.json['word'])
+        return jsonify({'words': newword}), 201
+    else:
+        return jsonify({"error": "PUT requests must be one word in length"}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
